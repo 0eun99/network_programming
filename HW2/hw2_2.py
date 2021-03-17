5379f7e861bb694e256ab7c5ce8cb7d5ecf50375
Choiye18@gmail.com <choiye18@gmail.com>
2021-03-17 15:17:57 +0900

    hw2

--- HW2/hw2_2.py
+++ HW2/hw2_2.py
@@ -1,23 +1,19 @@
-from random import randint
+A = eval(input('Enter number(A): '))
+B = eval(input('Enter number(B): '))
 
-start_coin = 50 #시작 코인
-large_coin = 100 #최대 코인
-small_coin = 0 #최소 코인
-guess = 0 #예상 숫자
+def gcd(x,y):
+    if x>y:
+        target = x
+        subtarget = y
+    else:
+        target = y
+        subtarget = x
 
-while start_coin <= large_coin and start_coin >= small_coin : 
-    coin = randint(1,2) #1 또는 2를 임의로 발생
-    guess = eval(input('Enter your guess(1,2): '))
-    if guess == coin: #숫자를 맞추면 9코인 얻고 틀리면 10코인 잃음
-        start_coin = start_coin + 9
-        print('맞았습니다!', start_coin)
-    
-    elif guess != coin:
-        start_coin = start_coin - 10
-        print('틀렸습니다!', start_coin)
+    Remaining = target % subtarget
+    if Remaining != 0:
+        return gcd(subtarget,Remaining)
 
-if start_coin <= 0 :
-    print('코인을 전부 잃었습니다.')
+    else:
+        return subtarget
 
-elif start_coin >= 100 :
-    print('축하합니다. 최대 코인을 획득했습니다.')
+print('최대 공약수 : ', gcd(A,B))
